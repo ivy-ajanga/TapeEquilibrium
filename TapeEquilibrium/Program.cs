21d7aa890cfc0e5f68d604ec4e65149b84a982ae
unknown <ajangaivy@gmail.com>
2020-08-20 14:57:47 +0300

    tapeequilibriumcodility

---
+++ TapeEquilibrium/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = { 3, 1, 2, 4, 3 };
+        Console.WriteLine(solution(arr));
+    }
+    public static int solution(int[] arr)
+    {
+        //if arraylength is 2 return the absolute diff of two elements
+        //loop through the array
+        //find abs diff head- tail
+        //head is sum of all arrays tail is subtr of all arrays each step
+        //compare abs diff with min diff
+        if (arr.Length == 2) return Math.Abs(arr[0] - arr[1]);
+        else
+        {
+            int head = arr[0];
+            int tail = 0;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                tail += arr[i];
+            }
+            int minimumdiff = Math.Abs(head - tail);
+            for (int i = 1; i < arr.Length; i++)
+            {
+                head += arr[i];
+                tail -= arr[i];
+                if (Math.Abs(head - tail) < minimumdiff)
+                {
+                    minimumdiff = Math.Abs(head - tail);
+                }
+            }
+            return minimumdiff;
+        }
+
+    }
+}
